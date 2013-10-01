50972c5f4a86b9b3fe882c7b5ac27e5d5bf7ccf1
Robert Martin <robertmartinbc@shaw.ca>
2013-09-30 21:02:35 -0700

    Implemented Cancan and basic roles.

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -9,10 +9,12 @@ class PromotionsController < ApplicationController
 
   def new
   	@promotion = Promotion.new
+    # authorize! :create, Promotion, message: "You need to be a memeber to create a new promotion."
   end
 
   def create
   @promotion = Promotion.new(params[:promotion])
+  # authorize! :create, @promotion, message: "You need to be signed up to do that."
   if @promotion.save
     flash[:notice] = "Promotion was saved."
     redirect_to @promotion
@@ -24,16 +26,18 @@ end
 
   def edit
     @promotion = Promotion.find(params[:id])
+    # authorize! :edit, @promotion, message: "You need to own the promotion to edit it."
   end
 
   def update
     @promotion = Promotion.find(params[:id])
+    # authorize! :update, @promotion, message: "You need to own the promotion to edit it."
     if @promotion.update_attributes(params[:promotion])
       flash[:notice] = "Promotion was updated."
       redirect_to @promotion
     else
       flash[:error] = "There was an error saving the promotion. Please try again."
-      render :new
+      render :edit
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131001020952) do
+ActiveRecord::Schema.define(:version => 20131001021749) do
 
   create_table "promotions", :force => true do |t|
     t.string   "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20131001020952) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
