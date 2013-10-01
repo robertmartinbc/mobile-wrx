454a2f4f50a4f12aaaecf2b2e7c72647c0948c06
Robert Martin <robertmartinbc@shaw.ca>
2013-09-30 17:29:10 -0700

    First commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileWrx::Application.config.secret_token = 'c52913c4fca16248bea1f15697a77308ff39ccade5d59b9f52bcc502ce48174562a792c8ca167f917be74efa9d0fda59dec27ae05bce2df0e1c2c0dad4ae715c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MobileWrx::Application.config.session_store :cookie_store, key: '_mobile-wrx_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MobileWrx::Application.config.session_store :active_record_store
